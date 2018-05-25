31eb05059fa255107df8bb032fa6dcf3fa0467c0
Vishnu Adda <addavishnu@gmail.com>
2018-05-24 22:55:47 -0700

    Made the app capable of iterating through one game.

--- app/src/main/java/com/someapp/vishnu/myhigherorlower/MainActivity.java
+++ app/src/main/java/com/someapp/vishnu/myhigherorlower/MainActivity.java
@@ -11,45 +11,47 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
+    Integer randomNumber = 0;
+
     public void onClick(View view){
-        
-        Random rand = new Random(); // Random object to call nextInt()
-        int randomNumber = rand.nextInt(20) + 1; // Put + 1 next to it for the minimum
 
-        while ( true ) {
-            Log.i("Info", "Button pressed");
+        Log.i("Info", "Button pressed");
+        if(randomNumber==0) {
+            Random rand = new Random(); // Random object to call nextInt()
+            randomNumber = rand.nextInt(20) + 1; // Put + 1 next to it for the minimum
+        }
 
-            EditText numberGuess = findViewById(R.id.numberEntered);
-            String guessString = numberGuess.toString();
+        EditText numberGuess = findViewById(R.id.numberEntered);
+        String guessString = numberGuess.getText().toString();
 
-            if (guessString.length() == 0) {
-                Toast.makeText(this, "Enter a number value before pressing the button",
-                        Toast.LENGTH_SHORT).show();
+        if (guessString.length() == 0) {
+            Toast.makeText(this, "Enter a number value before pressing the button",
+                    Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        for (int i = 0; i < guessString.length(); i++) {
+            if (!Character.isDigit(guessString.charAt(i))) {
+                Toast.makeText(this,
+                        "Only enter number values", Toast.LENGTH_SHORT).show();
                 return;
             }
-
-            for (int i = 0; i < guessString.length(); i++) {
-                if (!Character.isDigit(guessString.charAt(i))) {
-                    Toast.makeText(this,
-                            "Only enter number values", Toast.LENGTH_SHORT).show();
-                    return;
-                }
-            }
-
-            Log.i("Guess", guessString);
-
-            Integer guessValue = Integer.parseInt(guessString);
-//        int guessInt = guessValue;
-
-            if (guessValue < randomNumber) {
-                Toast.makeText(this, "Higher!", Toast.LENGTH_SHORT).show();
-            } else if (guessValue < randomNumber) {
-                Toast.makeText(this, "Lower!", Toast.LENGTH_SHORT).show();
-            } else {
-                Toast.makeText(this, "Correct!", Toast.LENGTH_SHORT).show();
-                break;
-            }
         }
+
+        Log.i("Guess", guessString);
+
+        Integer guessValue = Integer.parseInt(guessString);
+
+        if (guessValue < randomNumber) {
+            Toast.makeText(this, "Higher!", Toast.LENGTH_SHORT).show();
+        } else if (guessValue > randomNumber) {
+            Toast.makeText(this, "Lower!", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "Correct!" + " it was " +
+                            randomNumber.toString() + "."
+                        , Toast.LENGTH_SHORT).show();
+        }
+
 
 
     }
